1a5d00f0b8fb7abca23c38ffe02f1a94dc9f50c9
Rohitsj627 <rohitsj27@gmail.com>
2025-04-29 07:38:52 +0530

    df

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-gradient-to-t from-black to-gray-900 text-white py-8 mt-16">
+    <footer className="bg-gray-900 text-white py-8">
       <div className="max-w-7xl container mx-auto px-4 text-center">
         <p className="text-sm text-gray-400">
           Created by <span className="text-indigo-600">Bitwizrsj</span>. All rights reserved.
